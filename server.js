5b59c2cdcc2284eca9eb60735086c5d9408de00e
ulungfikri <ulung.fikri@gmail.com>
2020-03-03 10:44:16 +0700

    First Commit

---
+++ server.js
@@ -0,0 +1,28 @@
+var ftpClient = require('./lib/client.js'),
+    config = {
+        host: '',
+        port: 21,
+        user: '',
+        password: ''
+    },
+    options = {
+        logging: 'basic'
+    },
+    client = new ftpClient(config, options);
+
+client.connect(function () {
+
+    // client.upload(['test/**'], '/public_html/test', {
+    //     baseDir: 'test',
+    //     overwrite: 'older'
+    // }, function (result) {
+    //     console.log(result);
+    // });
+
+    client.download('./', 'D:/FB/UPLOAD_PATH/', {
+        overwrite: 'older'
+    }, function (result) {
+        console.log(result);
+    });
+
+});
